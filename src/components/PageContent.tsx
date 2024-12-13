185f674b735d07857adaad66f2b813ab3e88a075
motamarcelo <marcelomota.tech@gmail.com>
2024-12-13 10:30:04 -0300

    adiciona limite para o tamanho do conteudo

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -5,5 +5,5 @@ interface PageContentProps {
 }
 
 export default function PageContent({ children }: PageContentProps) {
-  return <div className="md:mx-20 xl:mx-60 mx-8 mt-36 mb-32 text-black">{children}</div>
+  return <div className="md:mx-20 xl:mx-auto max-w-4xl mx-8 mt-36 mb-32 text-black">{children}</div>
 }
