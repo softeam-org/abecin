ef47bff84c167a6a1596d04cd095c18796f8ab70
motamarcelo <marcelomota.tech@gmail.com>
2024-12-19 19:42:40 -0300

    fix: diminui o delay do hover do botao

--- src/components/publicacoes/ButtonIcon.tsx
+++ src/components/publicacoes/ButtonIcon.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function ButtonIcon({ title, icon, link }: Props) {
 	return (
 		<Link href={link}>
-			<button className="flex flex-row gap-2 items-center rounded-lg font-bold border-abecin-secondary text-abecin-secondary border-2 py-4 px-6 hover:text-white hover:bg-abecin-primary delay-100     ">
+			<button className="flex flex-row gap-2 items-center rounded-lg font-bold border-abecin-secondary text-abecin-secondary border-2 py-4 px-6 hover:text-white hover:bg-abecin-primary delay-75">
 				<i className={`fa-solid ${icon}`} />
 				{title}
 			</button>
